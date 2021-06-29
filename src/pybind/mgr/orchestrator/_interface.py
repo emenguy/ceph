sunilangadi2/ceph-ci

cephadm: expose gather-facts api method
Fixes: https://tracker.ceph.com/issues/51209

This PR intends to expose host metadata(gather-facts) api method

Signed-off-by: Avan Thakkar <athakkar@redhat.com>
Signed-off-by: Aashish Sharma <aasharma@redhat.com>

src/pybind/mgr/orchestrator/_interface.py
@@ -368,6 +368,12 @@ class Orchestrator(object):
         """
         raise NotImplementedError()
 
+    def get_gather_facts(self, hostname: Optional[str] = None) -> OrchResult[List[Dict[str, Any]]]:
+        """
+        Return hosts metadata(gather_facts).
+        """
+        raise NotImplementedError()
+
     def add_host_label(self, host: str, label: str) -> OrchResult[str]:
         """
         Add a host label

emenguy/ceph

src/pybind/mgr/orchestrator/_interface.py
@@ -368,6 +368,12 @@ class Orchestrator(object):
         """
         raise NotImplementedError()
 
+    def get_facts(self, hostname: Optional[str] = None) -> OrchResult[List[Dict[str, Any]]]:
+        """
+        Return hosts metadata(gather_facts).
+        """
+        raise NotImplementedError()
+
     def add_host_label(self, host: str, label: str) -> OrchResult[str]:
         """
         Add a host label